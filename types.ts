4331592ecf1175d0fb403ba4a84e5afadbe15c4c
luega <martinelliluca90@gmail.com>
2023-03-16 10:01:49 +0100

    DB:
    created fake db of patients

---
+++ types.ts
@@ -0,0 +1,10 @@
+type Patients = {
+  id: string;
+  name: string;
+  surname: string;
+  age: number;
+  gender: boolean; // if true male
+  phone: number;
+  address: string;
+  email: string;
+};
